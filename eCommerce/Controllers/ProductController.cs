8ccaa7ea1eb1401a49454fdff752c108e286b27f
UltProgrammerJune <jsimeon8620@students.cptc.edu>
2026-03-11 16:59:57 -0700

    Add async product listing page and navigation link
    Updated ProductController Index to fetch products asynchronously.
    Added currency display annotation to Product model.
    Added Product Listings link to main layout navigation.
    Created Index.cshtml to display products with create option.

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.Data;
 using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Controllers;
 
@@ -11,9 +12,10 @@ public class ProductController : Controller
     {
         _context = context;
     }
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<Product> allProducts = await _context.Products.ToListAsync();
+        return View(allProducts);
     }
 
     [HttpGet]
