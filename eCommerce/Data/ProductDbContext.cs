a8268d2f1de7ffe9443f3b1a611b7823136acacc
UltProgrammerJune <jsimeon8620@students.cptc.edu>
2026-03-11 13:58:15 -0700

    Add ProductDbContext for EF Core product tracking
    Introduced ProductDbContext in eCommerce.Data, inheriting from DbContext. Added DbSet<Product> to enable Entity Framework Core management of product entities. Constructor supports dependency injection via DbContextOptions.

---
+++ eCommerce/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using eCommerce.Models;
+
+namespace eCommerce.Data;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // Entities to be tracked by DbContext
+    public DbSet<Product> Products { get; set; }
+}
