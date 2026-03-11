ffdf35aeef32ee9ac07df750a5bdee2cff5b1b08
Jayden "June" Simeon <jsimeon8620@students.cptc.edu>
2026-03-11 13:39:27 -0700

    Add Product model, controller, and create view (#6)
    Introduced ProductController with index and create actions. Added Product model with validation attributes. Created Create.cshtml view for product creation.

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
