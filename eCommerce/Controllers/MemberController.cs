0cbfc53e38e807c3eab3dced2b7cfdc29021afd3
UltProgrammerJune <jsimeon8620@students.cptc.edu>
2026-03-12 13:39:28 -0700

    Add user registration feature with validation
    Introduced RegisterViewModel for registration form and validation. Added MemberController with Register action and Register.cshtml view. Updated site navigation to include "Register" link. Refactored Member class to separate registration-specific validation logic.

---
+++ eCommerce/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace eCommerce.Controllers;
+
+public class MemberController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -16,7 +16,7 @@ public class Member
     /// Public facing username of the member.
     /// Alphanumeric chars only.
     /// </summary>
-    [RegularExpression(@"^[a-zA-Z0-9]+$", 
+    [RegularExpression(@"^[a-zA-Z0-9]+$",
         ErrorMessage = "Username must be alphanumeric.")]
     [StringLength(25)]
     public required string Name { get; set; }
@@ -27,11 +27,31 @@ public class Member
     /// <summary>
     /// Password for the member's account.
     /// </summary>
-    [StringLength(50, MinimumLength = 6, 
+    [StringLength(50, MinimumLength = 6,
         ErrorMessage = "Your password must be between 6 and 50 characters.")]
     public required string Password { get; set; }
     /// <summary>
     /// Date of birth of the member.
     /// </summary>
+    public DateOnly DOB { get; set; }
+}
+
+public class RegisterViewModel
+{
+    [RegularExpression(@"^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric.")]
+    [StringLength(25)]
+    public required string Username { get; set; }
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Your password must be between 6 and 50 characters.")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    [Compare(nameof(Password),
+    ErrorMessage = "The password does not match.")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+    [DataType(DataType.Date)]
     public DateOnly DOB { get; set; }
 }
