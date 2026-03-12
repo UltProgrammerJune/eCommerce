67ba34d0edfed8268aa3b173d373ea7410179874
UltProgrammerJune <jsimeon8620@students.cptc.edu>
2026-03-12 12:55:53 -0700

    Add Member entity and migration to database schema
    Introduced a new "Member" entity with fields for Name, Email, Password, and DOB. Added migration to create the "Members" table with unique indexes on Name and Email. Updated model snapshot and designer files to reflect these changes. No modifications to the existing Product entity.

---
+++ eCommerce/Migrations/20260312195349_AddMember.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DOB = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_Email",
+                table: "Members",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_Name",
+                table: "Members",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
